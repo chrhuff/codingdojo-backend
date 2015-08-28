59bd8e3e41ce49c4443d47e6ff4f6b1b1d40ff8e
unknown <mosdoba@NB185.coinor.local>
2015-08-28 12:26:00 +0200

    basis skelett

---
+++ src/main/java/de/cofinpro/dojo/service/RestApplication.java
@@ -0,0 +1,13 @@
+package de.cofinpro.dojo.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * OrderRESTService Application
+ *
+ */
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
